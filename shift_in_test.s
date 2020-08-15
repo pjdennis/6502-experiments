f7fc9a484cad406def6da4f97a3674bf41c3e69f
Phil Dennis <pjdennis@gmail.com>
2020-08-15 11:30:48 -0500

    This works for read in by external CB1

--- shift_in_test.s
+++ shift_in_test.s
@@ -4,7 +4,11 @@ BUTTON          = %00100000
 SH_OUT_DATA     = %01000000
 SH_OUT_CLOCK    = %00010000
 
-IO_PINS_MASK    = BUTTON | SH_OUT_DATA | SH_OUT_CLOCK
+IO_PINS_MASK_A  = BUTTON | SH_OUT_DATA | SH_OUT_CLOCK
+
+SH_IN_CLOCK     = %10000000
+
+IO_PINS_MASK_B  = SH_IN_CLOCK
 
 D_S_I_P         = $00
 
@@ -22,11 +26,19 @@ program_entry:
   tsb PORTA
 
   lda DDRA
-  and #(~IO_PINS_MASK & $ff)
+  and #(~IO_PINS_MASK_A & $ff)
   ora #(SH_OUT_DATA | SH_OUT_CLOCK)
   sta DDRA
 
-  lda #ACR_SR_IN_T2
+  lda #SH_IN_CLOCK
+  tsb PORTB
+
+  lda DDRB
+  and #(~IO_PINS_MASK_B & $ff)
+  ora #(SH_IN_CLOCK)
+  sta DDRB
+
+  lda #ACR_SR_IN_CB1
   sta ACR
   lda #100
   sta T2CL
@@ -52,42 +64,32 @@ main_loop:
   lda #SH_OUT_DATA
   trb PORTA
 
-  lda SR   ; initialize read
+  ldx #8
+shift_in_loop:
+  jsr wait_for_button_down
+  lda #SH_IN_CLOCK
+  trb PORTB
+  jsr clear_display
+  jsr display_string_immediate
+  .asciiz "Clock low"
   jsr wait_for_button_up
 
-  
+  jsr wait_for_button_down
+  lda #SH_IN_CLOCK
+  tsb PORTB
+  jsr clear_display
+  jsr display_string_immediate
+  .asciiz "Clock high"
+  jsr wait_for_button_up
+
+  dex
+  bne shift_in_loop
+
   lda SR
   jsr clear_display
   jsr display_binary
 
   jmp stop_here
-
-
-  lda #0
-  jsr send_via_io_pins
-
-  jsr clear_display
-  jsr display_string_immediate
-  .asciiz "Message sent"
-
-  jsr wait_for_button_up
-  bra main_loop
-
-  jsr clear_display
-  jsr display_string_immediate
-  asciiz "ACR set to SR IN"
-
-  jsr wait_for_button_up
-
-
-  lda #DISPLAY_SECOND_LINE
-  jsr move_cursor
-
-;  lda #%10010101
-;  jsr display_binary
-
-
-;  jmp stop_here
 
 
   ldx #0
@@ -121,37 +123,37 @@ send_via_io_pins_loop:
   tax
   bcs send_via_io_pins_one
 ; send a zero
-  jsr wait_for_button_down
+;  jsr wait_for_button_down
   lda #SH_OUT_DATA
   trb PORTA
-  jsr wait_for_button_up
+;  jsr wait_for_button_up
 
-  jsr wait_for_button_down
+;  jsr wait_for_button_down
   lda #SH_OUT_CLOCK
   trb PORTA
-  jsr wait_for_button_up
+;  jsr wait_for_button_up
 
-  jsr wait_for_button_down
+;  jsr wait_for_button_down
   tsb PORTA
-  jsr wait_for_button_up
+;  jsr wait_for_button_up
 
   bra send_via_io_pins_continue
 
 send_via_io_pins_one
 ; send a one
-  jsr wait_for_button_down
+;  jsr wait_for_button_down
   lda #SH_OUT_DATA
   tsb PORTA
-  jsr wait_for_button_up
+;  jsr wait_for_button_up
 
-  jsr wait_for_button_down
+;  jsr wait_for_button_down
   lda #SH_OUT_CLOCK
   trb PORTA
-  jsr wait_for_button_up
+;  jsr wait_for_button_up
 
-  jsr wait_for_button_down
+;  jsr wait_for_button_down
   tsb PORTA
-  jsr wait_for_button_up
+;  jsr wait_for_button_up
 
 send_via_io_pins_continue:
   txa
