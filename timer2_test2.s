f72c638e8ef1ffb8fbfa6e6e295b425beeeaa659
Phil Dennis <pjdennis@gmail.com>
2021-12-04 16:03:07 -0600

    smaller test

---
+++ timer2_test2.s
@@ -0,0 +1,123 @@
+DELAY = 100
+
+DISPLAY_STRING_PARAM    = $00 ; 2 bytes
+
+T2_INT_COUNT_LOW        = $09 ; 1 byte
+T2_INT_COUNT_HIGH       = $0a ; 1 byte
+
+T2_INT_COUNT_LOW_COPY   = $13 ; 1 byte
+T2_INT_COUNT_HIGH_COPY  = $14 ; 1 byte
+
+  .include base_config_v1.inc
+
+  .org $2000
+
+  lda #<interrupt
+  sta RAM_IRQ_VECTOR + 0
+  lda #>interrupt
+  sta RAM_IRQ_VECTOR + 1
+
+  lda #<start_message
+  ldx #>start_message
+  jsr display_string
+
+  stz T2_INT_COUNT_LOW
+  stz T2_INT_COUNT_HIGH
+
+  lda #ACR_T2_TIMED ; Timer 2 one shot run mode
+  sta ACR
+
+  lda #<DELAY
+  sta T2CL
+  lda #>DELAY
+  sta T2CH
+
+  lda #(IERSETCLEAR | IT2) ; Enable timer 1 and 2 interrupts
+  sta IER
+
+  cli
+
+loop:
+  ; Copy data that is set by interrupt routines and reset timer done
+  sei
+  ldx T2_INT_COUNT_LOW
+  ldy T2_INT_COUNT_HIGH
+  cli
+
+  stx T2_INT_COUNT_LOW_COPY
+  sty T2_INT_COUNT_HIGH_COPY
+
+  lda #DISPLAY_FIRST_LINE + 7
+  jsr move_cursor
+
+  lda T2_INT_COUNT_HIGH_COPY
+  jsr display_hex
+  lda T2_INT_COUNT_LOW_COPY
+  jsr display_hex
+
+  ldx #0
+delay1:
+  ldy #0
+delay2:
+  pha
+  pla
+  dey
+  bne delay2
+  dex
+  bne delay1
+
+  jmp loop
+
+
+start_message: asciiz "Start."
+done_message:  asciiz " Done: "
+
+
+interrupt:
+  pha
+  phx
+  phy
+
+RESTART_OFFSET = 1
+RESTART_CYCLES = 33 + RESTART_OFFSET
+
+  ; Read from timer low counter which also clears the interrupt
+  ldx T2CL               ; *4 cycles - value read (when?)
+  ldy T2CH               ; *4 cycles
+
+  ; Adjust timer high if timer low is in range 0..3
+  txa                    ; *2 cycles
+  and #%11111100         ; *2 cycles
+                         ;   Adjust         No Adjust
+  bne timer_no_adjust    ;   2 cycles       3 cycles
+  iny                    ;   2 cycles       -
+  bra timer_ok           ;   3 cycles       -
+timer_no_adjust:         ;
+  nop                    ;   -              2 cycles
+  nop                    ;   -              2 cycles
+                         ; *7 cycles total
+timer_ok:
+
+ADJUSTED_DELAY = DELAY - RESTART_CYCLES
+
+  clc                    ; *2 cycles
+  txa                    ; *2 cycles
+  adc #<ADJUSTED_DELAY   ; *2 cycles
+  sta T2CL               ; *4 cycles
+  tya                    ; *2 cycles
+  adc #>ADJUSTED_DELAY   ; *2 cycles
+  sta T2CH               ; *4 cycles - restart timer (when?)
+
+  inc T2_INT_COUNT_LOW
+  bne inc_t2_count_done
+  inc T2_INT_COUNT_HIGH
+inc_t2_count_done:
+
+  ply
+  plx
+  pla
+  rti
+
+  .include display_update_routines_4bit.inc
+  .include display_hex.inc
+  .include display_string.inc
