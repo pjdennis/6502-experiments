7632d482a57d8c6d84b4b2c841654e356a10f23c
Phil Dennis <pjdennis@gmail.com>
2022-04-16 12:23:25 -0500

    This version causes display corruption when running at 4MHz

--- wendy2_relocate_test.s
+++ wendy2_relocate_test.s
@@ -4,6 +4,10 @@ CP_M_SRC_P           = $04   ; 2 bytes
 CP_M_LEN             = $06   ; 2 bytes
 
 ORIGIN               = $8000
+FIXED_RAM            = $4000
+FIXED_RAM_OFFSET     = ORIGIN - FIXED_RAM
+PROGRAM_LENGTH       = program_end - ORIGIN
+
 
   .include base_config_wendy2.inc
 
@@ -43,6 +47,9 @@ program_start:
   trb DISPLAY_ENABLE_PORT
   tsb DISPLAY_ENABLE_PORT + DDR_OFFSET
 
+  jmp switch_to_ram
+switch_to_ram_continue:
+
 
   jsr reset_and_enable_display_no_cursor
 
@@ -57,6 +64,59 @@ program_start:
   jsr display_string
 
   stp
+
+
+switch_to_ram:
+  ; copy PROGRAM_LENGTH bytes from program_start to FIXED_RAM
+  lda #<FIXED_RAM
+  sta CP_M_DEST_P
+  lda #>FIXED_RAM
+  sta CP_M_DEST_P + 1
+  lda #<ORIGIN
+  sta CP_M_SRC_P
+  lda #>ORIGIN
+  sta CP_M_SRC_P + 1
+  lda #<PROGRAM_LENGTH
+  sta CP_M_LEN
+  lda #>PROGRAM_LENGTH
+  sta CP_M_LEN + 1
+  jsr copy_memory
+
+  ; jmp to switch_to_ram_part_2 within FIXED_RAM (switch_to_ram_part_2 - FIXED_RAM_OFFSET)
+  jmp switch_to_ram_part_2 - FIXED_RAM_OFFSET
+
+
+switch_to_ram_part_2:
+  ; switch out ROM for RAM
+  lda #BANK_MASK
+  trb BANK_PORT
+  lda #BANK_START
+  tsb BANK_PORT
+
+  ldx #$ff                                 ; Initialize stack
+  txs
+
+  ; copy PROGRAM_LENGTH bytes from FIXED_RAM to program_start
+  lda #<ORIGIN
+  sta CP_M_DEST_P
+  lda #>ORIGIN
+  sta CP_M_DEST_P + 1
+  lda #<FIXED_RAM
+  sta CP_M_SRC_P
+  lda #>FIXED_RAM
+  sta CP_M_SRC_P + 1
+  lda #<PROGRAM_LENGTH
+  sta CP_M_LEN
+  lda #>PROGRAM_LENGTH
+  sta CP_M_LEN + 1
+  jsr copy_memory - FIXED_RAM_OFFSET
+
+  ; return to the calling code
+  jmp switch_to_ram_continue
+
+
+program_end:
+
 
   .org $fffc
   .word ORIGIN
