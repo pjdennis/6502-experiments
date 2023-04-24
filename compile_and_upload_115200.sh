74bdc9b746ebd08611d886f86917155904b616fe
Phil Dennis <pjdennis@gmail.com>
2023-04-23 21:49:56 -0500

    wendy2 upload script to new generic version

--- compile_and_upload_115200.sh
+++ compile_and_upload_115200.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-./vasm6502_oldstyle -wdc02 -wfail -Fbin -dotdir -ignore-mult-inc -esc $1 && python3 transfer_115200.py a.out
+./vasm6502_oldstyle -quiet -wdc02 -wfail -Fbin -dotdir -ignore-mult-inc -esc $1 && python3 transfer.py --port=/dev/cu.SLAB_USBtoUART --baudrate=115200 a.out
