84a76a4a2829918911e60933b6936c829464b54c
Phil Dennis <pjdennis@gmail.com>
2023-04-23 21:33:02 -0500

    Refactor to use new generic upload script

--- compile_and_upload_115200_wendy.sh
+++ compile_and_upload_115200_wendy.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-./vasm6502_oldstyle -wdc02 -wfail -Fbin -dotdir -ignore-mult-inc -esc $1 && python3 transfer_115200_wendy.py a.out
+./vasm6502_oldstyle -quiet -wdc02 -wfail -Fbin -dotdir -ignore-mult-inc -esc $1 && python3 transfer.py --noreset --port=/dev/cu.usbserial-1420 --baudrate=115200 a.out
