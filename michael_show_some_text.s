d57e1bb40e9d561cf5b2325762ac08bfeb9261ea
Phil Dennis <pjdennis@gmail.com>
2023-04-25 18:52:12 -0500

    Saving code that will word wrap text onto the screen
    TODO remove keyboard code

---
+++ michael_show_some_text.s
@@ -0,0 +1,398 @@
+  .include base_config_v2.inc
+
+INTERRUPT_ROUTINE        = $3f00
+
+TAB_WIDTH                = 4
+
+CP_M_DEST_P              = $00 ; 2 bytes
+CP_M_SRC_P               = $02 ; 2 bytes
+CP_M_LEN                 = $04 ; 2 bytes
+
+CREATE_CHARACTER_PARAM   = $06 ; 2 bytes
+
+SIMPLE_BUFFER_WRITE_PTR  = $08 ; 1 byte
+SIMPLE_BUFFER_READ_PTR   = $09 ; 1 byte
+
+DISPLAY_STRING_PARAM     = $0A ; 2 bytes
+TEXT_PTR                 = $0C ; 2 bytes
+TEXT_PTR_NEXT            = $0E ; 2 bytes
+SCROLL_OFFSET            = $10 ; 2 bytes
+LINE_CHARS_REMAINING     = $12 ; 1 byte
+MULTIPLY_8X8_RESULT_LOW  = $13 ; 1 byte
+MULTIPLY_8X8_TEMP        = $14 ; 1 byte
+
+GD_ZERO_PAGE_BASE        = $15 ; 18 bytes
+
+KB_ZERO_PAGE_BASE        = GD_ZERO_PAGE_STOP
+
+SIMPLE_BUFFER            = $0200 ; 256 bytes
+
+  .org $2000                     ; Loader loads programs to this address
+  jmp initialize_machine         ; Initialize hardware and then jump to program_start
+
+  ; The initialize_machine routine in this include will set up hardware registers and then
+  ; jump to program_start. We do not call a subroutine because for some machine designs the
+  ; stack is not usable until after the hardware registers have been initialized
+  .include delay_routines.inc
+  .include initialize_machine_v2.inc
+EXTEND_CHARACTER_SET = 1
+  .include display_routines.inc
+  .include display_string.inc
+  .include simple_buffer.inc
+  .include copy_memory.inc
+  .include key_codes.inc
+  .include keyboard_typematic.inc
+KB_BUFFER_INITIALIZE = simple_buffer_initialize
+KB_BUFFER_WRITE      = simple_buffer_write
+KB_BUFFER_READ       = simple_buffer_read
+  .include keyboard_driver.inc
+  .include display_hex.inc
+  .include multiply8x8.inc
+  .include graphics_display.inc
+
+program_start:
+  ; Initialize stack
+  ldx #$ff
+  txs
+
+  jsr gd_configure
+  jsr gd_reset
+  jsr gd_select
+  jsr gd_initialize
+  lda #ILI9341_MADCTL
+  jsr gd_send_command
+  lda #%10101000    ; original $48
+  jsr gd_send_data
+  jsr gd_clear_screen
+  jsr gd_unselect
+
+  stz GD_ROW
+  stz GD_COL
+
+  jsr gd_select
+
+  jsr show_some_text
+
+  jsr gd_unselect
+
+  stp
+
+;  stz GD_ROW
+;  jsr gd_clear_line
+;  lda #1
+
+
+;SCROLL_MAX = ILI9341_TFTHEIGHT - 16
+;  lda #<SCROLL_MAX
+;  sta SCROLL_OFFSET
+;  lda #>SCROLL_MAX
+;  sta SCROLL_OFFSET + 1
+;.scroll_loop:
+;; send command
+;  lda #ILI9341_VSCRSADD
+;  jsr gd_send_command
+;  lda SCROLL_OFFSET + 1
+;  jsr gd_send_data
+;  lda SCROLL_OFFSET
+;  jsr gd_send_data
+;  lda #50
+;  jsr delay_hundredths
+;; check for end
+;  lda SCROLL_OFFSET
+;  bne .scroll_offset_ok
+;  lda SCROLL_OFFSET + 1
+;  bne .scroll_offset_ok
+;  lda #<SCROLL_MAX
+;  sta SCROLL_OFFSET
+;  lda #>SCROLL_MAX
+;  sta SCROLL_OFFSET + 1
+;  bra .scroll_loop
+;.scroll_offset_ok:
+;; decrement offset
+;  sec
+;  lda SCROLL_OFFSET
+;  sbc #16
+;  sta SCROLL_OFFSET
+;  lda SCROLL_OFFSET + 1
+;  sbc #0
+;  sta SCROLL_OFFSET + 1
+;  bra .scroll_loop
+
+;  lda #GD_CHAR_ROWS - 2
+;  sta GD_ROW
+
+  lda #'_'
+  jsr gd_show_character
+
+  jsr gd_unselect
+
+  jsr reset_and_enable_display_no_cursor
+  lda #<start_message
+  ldx #>start_message
+  jsr display_string
+
+  jsr keyboard_initialize
+
+  ; Read and display translated characters from the keyboard
+
+  ldx #0
+get_char_loop:
+  cpx #0
+  bne .not_off
+  jsr gd_select
+  lda #' '
+  jsr gd_show_character
+  jsr gd_unselect
+.not_off:
+  cpx #25
+  bne .not_on
+  jsr gd_select
+  lda #'_'
+  jsr gd_show_character
+  jsr gd_unselect
+.not_on:
+  inx
+  cpx #50
+  bne .no_reset_count
+  ldx #0
+.no_reset_count:
+  lda #1
+  jsr delay_hundredths
+  jsr keyboard_get_char
+  bcs get_char_loop
+get_char_loop_2:
+  jsr callback_char_received
+  jsr keyboard_get_char
+  bcc get_char_loop_2
+  jsr callback_no_more_chars
+  bra get_char_loop
+
+
+start_message: .asciiz "Last key press:"
+
+
+callback_char_received:
+  phx
+  phy
+  pha
+  lda #DISPLAY_SECOND_LINE
+  jsr move_cursor
+  pla
+  pha
+  jsr display_character
+  lda #' '
+  jsr display_character
+  pla
+  pha
+  jsr display_hex
+  jsr gd_select
+  pla
+  cmp #$08
+  beq .backspace
+  cmp #$09
+  beq .tab
+  cmp #$0a
+  beq .newline
+  jsr gd_show_character
+  lda GD_ROW
+  cmp #GD_CHAR_ROWS - 1
+  bne .not_last_char
+  lda GD_COL
+  cmp #GD_CHAR_COLS - 1
+  bne .not_last_char
+  jsr do_scroll
+  bra .done
+.not_last_char:
+  jsr gd_next_character
+  bra .done
+.backspace:
+  lda GD_ROW
+  bne .not_first_char
+  lda GD_COL
+  beq .return
+.not_first_char:
+  lda #' '
+  jsr gd_show_character
+  jsr gd_previous_character
+  bra .done
+.tab:
+  jsr do_tab
+  bra .done
+.newline:
+  lda #' '
+  jsr gd_show_character
+  lda GD_ROW
+  cmp #GD_CHAR_ROWS - 1
+  bne .not_last_line
+  jsr do_scroll
+  bra .done
+.not_last_line:  
+  jsr gd_next_line
+.done:
+  lda #'_'
+  jsr gd_show_character
+.return
+  jsr gd_unselect
+  ply
+  plx
+  rts
+
+
+do_tab:
+  lda #' '
+  jsr gd_show_character
+  lda #TAB_WIDTH
+.loop:
+  cmp #GD_CHAR_COLS
+  bcs .next_line
+  cmp GD_COL
+  beq .over1
+  bcs .move_cursor ; A > GD_COL
+.over1
+  clc
+  adc #TAB_WIDTH
+  bra .loop
+.next_line
+  lda GD_ROW
+  cmp #GD_CHAR_ROWS - 1
+  bne .not_last_line
+  jmp do_scroll ; tail call
+.not_last_line:
+  jmp gd_next_line ; tail call
+.move_cursor:
+  sta GD_COL
+  rts
+
+
+do_scroll:
+  stz GD_ROW
+  jsr gd_clear_line
+  lda #1
+  jsr gd_scroll_up
+  lda #GD_CHAR_ROWS - 1
+  sta GD_ROW
+  stz GD_COL
+  rts
+
+
+callback_no_more_chars:
+  rts
+
+
+callback_key_left:
+  rts
+
+
+callback_key_right:
+  rts
+
+
+callback_key_esc:
+  rts
+
+
+callback_key_f1:
+  ldy #0
+.loop:
+  lda .f1_text, Y
+  beq .done
+  jsr callback_char_received
+  iny
+  bra .loop
+.done:
+  rts
+
+.f1_text: .asciiz "The quick brown fox jumps over the lazy dog. "
+
+
+show_some_text:
+  lda #<message_text
+  sta TEXT_PTR
+  lda #>message_text
+  sta TEXT_PTR + 1
+  lda #GD_CHAR_COLS
+  sta LINE_CHARS_REMAINING
+.show_spaces_loop:
+  lda LINE_CHARS_REMAINING
+  beq .skip_remaining_spaces
+  lda (TEXT_PTR)
+  beq .done
+  cmp #' '
+  bne .show_text
+  jsr gd_show_character
+  jsr gd_next_character
+  dec LINE_CHARS_REMAINING
+  inc TEXT_PTR
+  bne .show_spaces_loop
+  inc TEXT_PTR + 1
+  bra .show_spaces_loop
+.show_text:
+  lda (TEXT_PTR)
+  beq .done
+; look for end of word
+  ldy #1
+.word_search_loop:
+  lda (TEXT_PTR),Y
+  beq .found_word_end
+  cmp #' '
+  beq .found_word_end
+  iny
+  cpy #GD_CHAR_COLS
+  bne .word_search_loop
+.found_word_end:
+; will word fit on line?
+  cpy LINE_CHARS_REMAINING
+  bcc .word_fits
+  beq .word_fits
+; word does not fit
+.finish_line_loop:
+  lda #' '
+  jsr gd_show_character
+  jsr gd_next_character
+  dec LINE_CHARS_REMAINING
+  bne .finish_line_loop
+  lda #GD_CHAR_COLS
+  sta LINE_CHARS_REMAINING
+.word_fits:
+.show_word_loop:
+  lda (TEXT_PTR)
+  jsr gd_show_character
+  jsr gd_next_character
+  dec LINE_CHARS_REMAINING
+  inc TEXT_PTR
+  bne .over
+  inc TEXT_PTR + 1
+.over:
+  dey
+  bne .show_word_loop
+  bra .show_spaces_loop
+.skip_remaining_spaces:
+  lda #GD_CHAR_COLS
+  sta LINE_CHARS_REMAINING
+.skip_spaces_loop:
+  lda (TEXT_PTR)
+  beq .done
+  cmp #' '
+  bne .show_text
+  inc TEXT_PTR
+  bne .skip_spaces_loop
+  inc TEXT_PTR + 1
+  bra .skip_spaces_loop
+.done:
+  lda LINE_CHARS_REMAINING
+  cmp #GD_CHAR_COLS
+  beq .exit
+.finish_last_line_loop:
+  lda #' '
+  jsr gd_show_character
+  jsr gd_next_character
+  dec LINE_CHARS_REMAINING
+  bne .finish_last_line_loop
+.exit:
+  rts
+
+message_text: .asciiz "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+
+;message_text: .asciiz "  a 1234567890123456789 b 12345678901234567890 c 123456789012345678901 d e the quick brown fox. 123456789"
+
+
