949b4d589d3d5577577428880a5ddcb4442aafae
Phil Dennis <pjdennis@gmail.com>
2023-05-28 19:02:23 +0500

    data display

---
+++ assembler/hexdump3.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+
+#define BYTES_PER_LINE 32
+
+void hexdump(FILE *fp) {
+    unsigned char buffer[BYTES_PER_LINE];
+    size_t n;
+    unsigned long long addr = 0;
+    int i;
+
+    while ((n = fread(buffer, 1, BYTES_PER_LINE, fp)) > 0) {
+        printf("%08llx  ", addr);
+        for (i = 0; i < n; ++i) {
+            printf("%02x ", buffer[i]);
+            if (i == BYTES_PER_LINE/4-1 || i == BYTES_PER_LINE/4*2-1 || i == BYTES_PER_LINE/4*3-1)
+                printf(" ");
+        }
+
+        if (n < BYTES_PER_LINE) {
+            for (i = n; i < BYTES_PER_LINE; ++i) {
+                printf("   ");
+                if (i == BYTES_PER_LINE/4-1 || i == BYTES_PER_LINE/4*2-1 || i == BYTES_PER_LINE/4*3-1)
+                    printf(" ");
+            }
+        }
+
+        printf(" |"); // Reduced the number of spaces here.
+        for (i = 0; i < n; ++i) {
+            if (buffer[i] >= 32 && buffer[i] <= 126)
+                printf("%c", buffer[i]);
+            else
+                printf(".");
+        }
+        printf("|\n");
+
+        addr += n;
+    }
+}
+
+int main(int argc, char *argv[]) {
+    if (argc < 2) {
+        fprintf(stderr, "Usage: %s <file>\n", argv[0]);
+        return 1;
+    }
+
+    FILE *fp = fopen(argv[1], "rb");
+    if (fp == NULL) {
+        perror("Unable to open file");
+        return 1;
+    }
+
+    hexdump(fp);
+    fclose(fp);
+
+    return 0;
+}
--- assembler/sidebyside.cpp
+++ assembler/sidebyside.cpp
@@ -3,23 +3,23 @@
 #include <string>
 
 int main() {
-  const int columns = 3;
+    const int columns = 3;
 
-  std::vector<std::string> lines;
+    std::vector<std::string> lines;
 
-  for (std::string line; std::getline(std::cin, line); ) {
-    lines.push_back(line);
-  }
-
-  int rows_per_column = (lines.size() + columns - 1) / columns; // integer division rounds up
-
-  for (int row = 0; row < rows_per_column; ++row) {
-    std::cout << lines[row];
-    for (int col = 1; col < columns; ++col) {
-      int i = rows_per_column * col + row;
-      if (i == lines.size()) break;
-      std::cout << "  " << lines[i];
+    for (std::string line; std::getline(std::cin, line); ) {
+        lines.push_back(line);
     }
-    std::cout << "\n";
-  }
+
+    int rows_per_column = (lines.size() + columns - 1) / columns; // integer division rounds up
+
+    for (int row = 0; row < rows_per_column; ++row) {
+        std::cout << lines[row];
+        for (int col = 1; col < columns; ++col) {
+            int i = rows_per_column * col + row;
+            if (i == lines.size()) break;
+            std::cout << "  " << lines[i];
+        }
+        std::cout << "\n";
+    }
 }
