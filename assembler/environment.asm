3ce59c3fb5b5881321336ec187f1e6aaf43179e7
Phil Dennis <pjdennis@gmail.com>
2025-11-12 22:06:54 -0600

    Updated documentation

--- assembler/environment.asm
+++ assembler/environment.asm
@@ -1,12 +1,15 @@
 ; Provided by environment:
-read_b    = $F006 ; Returns next char in A; C set when at end
-write_b   = $F009 ; Writes char to stdout
-write_d   = $F00C ; Writes char to stderr
+read_b    = $F006 ; Returns next char in A; C set when at end; X, Y preserved
+write_b   = $F009 ; Writes char in A to stdout; A, X, Y preserved
+write_d   = $F00C ; Writes char in A to stderr; A, X, Y preserved
 exit      = $F00F ; Exits the program; exit code in A
-open      = $F012 ; Opens file with name at A;X. Returns handle in A
-close     = $F015 ; Closes file with handle in A
-read      = $F018 ; Reads from file with handle in A
-argc      = $F01B ; Returns argument count in A
-argv      = $F01E ; Returns argument A in A;X
-openout   = $F021
-write     = $F024
+open      = $F012 ; Opens file with name at A;X for reading. Returns handle
+                  ; in A; Y preserved
+close     = $F015 ; Closes file with handle in A; A, X, Y preserved
+read      = $F018 ; Reads from file with handle in A; returns next char in A;
+                  ; C set when at end; X, Y preserved
+argc      = $F01B ; Returns argument count in A; X, Y preserved
+argv      = $F01E ; Returns argument A in A;X; Y preserved
+openout   = $F021 ; Opens file with name at A;X for writing. Returns handle
+                  ; in A; Y preserved
+write     = $F024 ; writs char in A to file with handle in X; Y preserved
