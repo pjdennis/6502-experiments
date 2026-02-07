9b1bb242e7b6d628960832473be7855643badea5
Phil Dennis <pjdennis@gmail.com>
2026-02-07 15:01:27 -0600

    Unify escape decoding

--- assembler2/22/asm.asm
+++ assembler2/22/asm.asm
@@ -375,6 +375,48 @@ read_filename
 ; Parse values: hex, labels, character literals
 ; ============================================================================
 
+; Decode escape sequence character (after backslash)
+; On entry: A contains the escape code character
+; On exit: A contains decoded value if recognized
+;          C = 1 if recognized, C = 0 otherwise
+;          X, Y are preserved
+decode_escape
+  CMP #'n'
+  BNE .esc_not_n
+  LDA #'\n'            ; Linefeed
+  SEC
+  RTS
+.esc_not_n
+  CMP #'b'
+  BNE .esc_not_b
+  LDA #$08             ; Backspace
+  SEC
+  RTS
+.esc_not_b
+  CMP #'t'
+  BNE .esc_not_t
+  LDA #$09             ; Tab
+  SEC
+  RTS
+.esc_not_t
+  CMP #'r'
+  BNE .esc_not_r
+  LDA #$0D             ; Carriage return
+  SEC
+  RTS
+.esc_not_r
+  CMP #'\\'
+  BEQ .esc_same
+  CMP #'\''
+  BEQ .esc_same
+  CMP #'"'
+  BEQ .esc_same
+  CLC
+  RTS
+.esc_same
+  SEC
+  RTS
+
 ; Parse character literal: 'x' or escape sequences
 ; On entry: A contains the opening quote character '
 ; On exit: A contains current character (for garbage checking)
@@ -394,34 +436,9 @@ parse_char_literal
   STA OPERAND16
   JMP .char_check_close
 .char_escape
-  ; Escape sequence: \b \t \n \r \\ \'
   JSR read_char
-  CMP #'n'
-  BNE .esc_not_n
-  LDA #'\n'            ; Only \n needs value substitution
-  BNE .esc_done        ; Always taken (\n = $0A != 0)
-.esc_not_n
-  CMP #'b'
-  BNE .esc_not_b
-  LDA #$08             ; Backspace
-  BNE .esc_done        ; Always taken ($08 != 0)
-.esc_not_b
-  CMP #'t'
-  BNE .esc_not_t
-  LDA #$09             ; Tab
-  BNE .esc_done        ; Always taken ($09 != 0)
-.esc_not_t
-  CMP #'r'
-  BNE .esc_not_r
-  LDA #$0D             ; Carriage return
-  BNE .esc_done        ; Always taken ($0D != 0)
-.esc_not_r
-  ; For \\ and \', character is already in A
-  CMP #'\\'
-  BEQ .esc_done
-  CMP #'\''
-  BNE .char_invalid
-.esc_done
+  JSR decode_escape
+  BCC .char_invalid
   STA OPERAND16
 .char_check_close
   JSR read_char        ; Should be closing quote
@@ -1346,25 +1363,7 @@ emit_quoted
   JSR read_char
   CMP #'\n'
   BEQ .err_closing_quote
-  CMP #'n'
-  BNE .esc_check_b
-  LDA #'\n'            ; Escaped "n" is linefeed
-  BNE .escaped_done    ; Always taken
-.esc_check_b
-  CMP #'b'
-  BNE .esc_check_t
-  LDA #$08             ; Escaped "b" is backspace
-  BNE .escaped_done    ; Always taken
-.esc_check_t
-  CMP #'t'
-  BNE .esc_check_r
-  LDA #$09             ; Escaped "t" is tab
-  BNE .escaped_done    ; Always taken
-.esc_check_r
-  CMP #'r'
-  BNE .not_escaped
-  LDA #$0D             ; Escaped "r" is carriage return
-.escaped_done
+  JSR decode_escape
 .not_escaped
   JSR emit
   JSR read_char
