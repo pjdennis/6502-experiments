37a0ab39b075e2a10ac6ec36035ff17b4f56b706
Phil Dennis <pjdennis@gmail.com>
2026-02-08 13:14:16 -0600

    Refactor macros.asm to use decimal constants

--- assembler2/23/macros.asm
+++ assembler2/23/macros.asm
@@ -4,22 +4,22 @@
 ; (low byte at ptr, high byte at ptr+$01)
 
 
-; INC16 ptr - Increment 16-bit value at ptr/ptr+$01
+; INC16 ptr - Increment 16-bit value at ptr/ptr + 1
 ; Preserves A, X, Y
   .macro INC16 ptr
   INC ptr
   BNE .skip
-  INC ptr+$01
+  INC ptr + 1
 .skip:
   .endmacro
 
 
-; DEC16 ptr - Decrement 16-bit value at ptr/ptr+$01
+; DEC16 ptr - Decrement 16-bit value at ptr/ptr + 1
 ; Preserves X, Y
   .macro DEC16 ptr
   LDA ptr
   BNE .skip
-  DEC ptr+$01
+  DEC ptr + 1
 .skip:
   DEC ptr
   .endmacro
@@ -31,9 +31,9 @@
   LDA ptr1
   ADC ptr2
   STA ptr3
-  LDA ptr1+$01
-  ADC ptr2+$01
-  STA ptr3+$01
+  LDA ptr1 + 1
+  ADC ptr2 + 1
+  STA ptr3 + 1
   .endmacro
 
 
@@ -42,9 +42,9 @@
   .macro ADCA16 ptr1, ptr2
   ADC ptr1
   STA ptr2
-  LDA #$00
-  ADC ptr1+$01
-  STA ptr2+$01
+  LDA #0
+  ADC ptr1 + 1
+  STA ptr2 + 1
   .endmacro
 
 
@@ -54,9 +54,9 @@
   LDA ptr1
   ADC #<val
   STA ptr2
-  LDA ptr1+$01
+  LDA ptr1 + 1
   ADC #>val
-  STA ptr2+$01
+  STA ptr2 + 1
   .endmacro
 
 
@@ -66,9 +66,9 @@
   LDA ptr1
   SBC ptr2
   STA ptr3
-  LDA ptr1+$01
-  SBC ptr2+$01
-  STA ptr3+$01
+  LDA ptr1 + 1
+  SBC ptr2 + 1
+  STA ptr3 + 1
   .endmacro
 
 
@@ -78,9 +78,9 @@
   LDA ptr1
   SBC #<val
   STA ptr2
-  LDA ptr1+$01
+  LDA ptr1 + 1
   SBC #>val
-  STA ptr2+$01
+  STA ptr2 + 1
   .endmacro
 
 
@@ -90,8 +90,8 @@
 ;   BCC branches if value at ptr1 < value at ptr2
 ; Clobbers A
   .macro CMP16 ptr1, ptr2
-  LDA ptr1+$01
-  CMP ptr2+$01
+  LDA ptr1 + 1
+  CMP ptr2 + 1
   BNE .done
   LDA ptr1
   CMP ptr2
@@ -105,7 +105,7 @@
 ;   BCC branches if value at ptr < val
 ; Clobbers A
   .macro CMPI16 ptr, val
-  LDA ptr+$01
+  LDA ptr + 1
   CMP #>val
   BNE .done
   LDA ptr
@@ -125,13 +125,13 @@
 ; ASL16 ptr - Shift 16 bit value at ptr left
   .macro ASL16 ptr
   ASL ptr
-  ROL ptr+$01
+  ROL ptr + 1
   .endmacro
 
 
 ; LSR16 ptr - Shift 16 bit value at ptr right
   .macro LSR16 ptr
-  LSR ptr+$01
+  LSR ptr + 1
   ROR ptr
   .endmacro
 
@@ -139,13 +139,13 @@
 ; ROL16 ptr - Rotate 16 bit value at ptr left
   .macro ROL16 ptr
   ROL ptr
-  ROL ptr+1
+  ROL ptr + 1
   .endmacro
 
 
 ; ROR16 ptr - Rotate 16 bit value at ptr right
   .macro ROR16 ptr
-  ROR ptr+1
+  ROR ptr + 1
   ROR ptr
   .endmacro
 
@@ -155,18 +155,18 @@
   .macro CP16 src, dst
   LDA src
   STA dst
-  LDA src+$01
-  STA dst+$01
+  LDA src + 1
+  STA dst + 1
   .endmacro
 
 
-; SET16 val ptr - Load 16-bit immediate value into ptr/ptr+$01
+; SET16 val ptr - Load 16-bit immediate value into ptr/ptr + 1
 ; Clobbers A
   .macro SET16 val, ptr
   LDA #<val
   STA ptr
   LDA #>val
-  STA ptr+$01
+  STA ptr + 1
   .endmacro
 
 
@@ -175,7 +175,7 @@
 ; Preserves A, X, Y
   .macro STA_LH16 ptr
   STA ptr
-  STA ptr+$01
+  STA ptr + 1
   .endmacro
 
 
@@ -184,7 +184,7 @@
   .macro PUSH16 ptr
   LDA ptr
   PHA
-  LDA ptr+$01
+  LDA ptr + 1
   PHA
   .endmacro
 
@@ -193,7 +193,7 @@
 ; Clobbers A
   .macro POP16 ptr
   PLA
-  STA ptr+$01
+  STA ptr + 1
   PLA
   STA ptr
   .endmacro
@@ -203,12 +203,12 @@
 ; Used before calling subroutines that take A/X as 16-bit parameter
   .macro LDAX16 ptr
   LDA ptr
-  LDX ptr+$01
+  LDX ptr + 1
   .endmacro
 
 
 ; STAX16 ptr - Store A (low) / X (high) into 16-bit pointer
   .macro STAX16 ptr
   STA ptr
-  STX ptr+$01
+  STX ptr + 1
   .endmacro
