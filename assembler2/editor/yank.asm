2d8d9f566dee42a32bdc4c1bf61bc25de833ecb9
Phil Dennis <pjdennis@gmail.com>
2026-02-09 06:51:14 -0600

    Optimize editor code for size by calling the 'n' times operation with 1 vs. having a separate implemenation for 1 time.

--- assembler2/editor/yank.asm
+++ assembler2/editor/yank.asm
@@ -88,92 +88,9 @@ yank_get_size:
 ; Sets cursor to first pasted line, col 0
 ; Returns carry set = buffer full or empty yank, carry clear = success
 yank_paste_below:
-  JSR yank_get_size
-  BCS .paste_empty           ; Nothing to paste
-
-  ; Find insertion point: after current line's newline
-  LDAX16 FILE_LINE16
-  JSR buf_get_line_ptr       ; BUF_PTR16 = start of current line
-  LDY #0
-.find_nl_below:
-  LDA (BUF_PTR16),Y
-  CMP #'\n'
-  BEQ .found_nl_below
-  INY
-  BNE .find_nl_below
-.found_nl_below:
-  ; BUF_PTR16 + Y + 1 = insertion point (after newline)
-  INY
-  TYA
-  CLC
-  ADCA16 BUF_PTR16, BUF_PTR16
-
-  ; Set up source and size for buf_insert_block
-  SET16 YANK_BUF, BUF_SRC16
-  ; BUF_LEN16 already set by yank_get_size
-
-  JSR buf_insert_block
-  BCS .paste_full
-
-  JSR buf_rebuild_lines
-
-  ; Move cursor to first pasted line
-  INC16 FILE_LINE16
-  LDA #0
-  STA CURSOR_COL
-  JSR ensure_cursor_visible
-  JSR clamp_cursor_col
-  CLC
-  RTS
-
-.paste_full:
-  SET16 str_buffer_full, STR_PTR16
-  JSR show_status_message
-  SEC
-  RTS
-
-.paste_empty:
-  SEC
-  RTS
-
-; Paste yank buffer above current line
-; Inserts yank content at start of current line
-; Sets cursor to first pasted line (same line number), col 0
-; Returns carry set = buffer full or empty yank, carry clear = success
-yank_paste_above:
-  JSR yank_get_size
-  BCS .paste_above_empty     ; Nothing to paste
-
-  ; Insertion point: start of current line
-  LDAX16 FILE_LINE16
-  JSR buf_get_line_ptr       ; BUF_PTR16 = start of current line
-
-  ; Set up source and size for buf_insert_block
-  SET16 YANK_BUF, BUF_SRC16
-  ; BUF_LEN16 already set by yank_get_size
-
-  JSR buf_insert_block
-  BCS .paste_above_full
-
-  JSR buf_rebuild_lines
-
-  ; Cursor stays at same line number (which is now the first pasted line)
-  LDA #0
-  STA CURSOR_COL
-  JSR ensure_cursor_visible
-  JSR clamp_cursor_col
-  CLC
-  RTS
-
-.paste_above_full:
-  SET16 str_buffer_full, STR_PTR16
-  JSR show_status_message
-  SEC
-  RTS
-
-.paste_above_empty:
-  SEC
-  RTS
+  LDA #1
+  STA BUF_TEMP
+  ; Fall through
 
 ; Paste yank buffer below current line, N times in one batch operation
 ; Input: BUF_TEMP = count of times to paste
@@ -262,6 +179,15 @@ yank_paste_below_n:
   JSR clamp_cursor_col
   CLC
   RTS
+
+; Paste yank buffer above current line
+; Inserts yank content at start of current line
+; Sets cursor to first pasted line (same line number), col 0
+; Returns carry set = buffer full or empty yank, carry clear = success
+yank_paste_above:
+  LDA #1
+  STA BUF_TEMP
+  ; Fall through
 
 ; Paste yank buffer above current line, N times in one batch operation
 ; Input: BUF_TEMP = count of times to paste
