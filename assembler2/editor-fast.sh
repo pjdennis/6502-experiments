1b12293f6909f3fede627be244db89b289f52a91
Phil Dennis <pjdennis@gmail.com>
2026-02-14 13:53:48 -0600

    Reorganize editor scripts. Default is now the 19200 baud terminal mode editor

---
+++ assembler2/editor-fast.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
+exec "$SCRIPT_DIR/emulator.out" "$SCRIPT_DIR/editor/out/editor_stable.out" --load 0400 --console --mhz 2 "$@"
--- assembler2/editor-terminal.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
-exec "$SCRIPT_DIR/emulator.out" "$SCRIPT_DIR/editor/out/editor_terminal.out" --load 0400 --terminal --baud 19200 --mhz 2 "$@"
