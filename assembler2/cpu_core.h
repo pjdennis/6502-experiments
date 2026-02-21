4576bcacd975e9b1c8945ea1090a0ce80a55af09
Phil Dennis <pjdennis@gmail.com>
2026-02-21 08:13:00 -0600

    Extract Fake6502 CPU engine into cpu_core.c/.h

---
+++ assembler2/cpu_core.h
@@ -0,0 +1,26 @@
+#ifndef CPU_CORE_H
+#define CPU_CORE_H
+
+#include <stdint.h>
+
+// 6502 CPU registers
+extern uint16_t pc;
+extern uint8_t sp, a, x, y, status;
+
+// Helper variables
+extern uint32_t instructions;
+extern uint64_t clockticks6502, clockgoal6502;
+
+// CPU interface
+void reset6502(void);
+void exec6502(uint64_t tickcount);
+void step6502(void);
+void nmi6502(void);
+void irq6502(void);
+void hookexternal(void *funcptr);
+
+// These must be provided by the host
+extern uint8_t read6502(uint16_t address);
+extern void write6502(uint16_t address, uint8_t value);
+
+#endif
